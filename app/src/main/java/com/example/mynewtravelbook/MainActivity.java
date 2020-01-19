d3c8f8bddaf455ddf839d82a19cf4346976f848c
kaankizilagil <kaankizilagil@gmail.com>
2020-01-19 14:38:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynewtravelbook/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.mynewtravelbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> names = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<LatLng>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_place, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.add_place) {
+
+            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            intent.putExtra("information","new");
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        try {
+
+            MapsActivity.database = this.openOrCreateDatabase("Places", MODE_PRIVATE,null);
+            Cursor cursor = MapsActivity.database.rawQuery("SELECT * FROM places",null);
+
+            int nameIdx          = cursor.getColumnIndex("name");
+            int latitudeIdx      = cursor.getColumnIndex("latitude");
+            int longitudeIdx     = cursor.getColumnIndex("longitude");
+
+            while (cursor.moveToNext()) {
+
+                String nameFromDatabase         = cursor.getString(nameIdx);
+                String latitudeFromDatabase     = cursor.getString(latitudeIdx);
+                String longitudeFromDatabase    = cursor.getString(longitudeIdx);
+
+                names.add(nameFromDatabase);
+
+                Double l1 = Double.parseDouble(latitudeFromDatabase);
+                Double l2 = Double.parseDouble(longitudeFromDatabase);
+
+                LatLng locationFromDatabase = new LatLng(l1,l2);
+
+                locations.add(locationFromDatabase);
+            }
+
+            cursor.close();
+        }
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("information","old");
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+    }
+}
